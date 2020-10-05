e869f9067f0bcf0e6ca5b62e68be18787014e680
svmcd <spolat0750@gmail.com>
2020-10-05 14:25:54 +0200

    new files

--- DeeplyNested.py
+++ DeeplyNested.py
@@ -1,7 +1,29 @@
-def goWandelen():
-	print("Lekker wandelen!")
+Honger = True
+zinInKoken = False
+kliekje = False
+geld = 9
 
-def goFietsen
-	print("Lekker fietsen!")
+def goKook() :
+    print("Je gaat koken!")
 
+def eetKliekje() :
+    print("Je eet een kliekje")
+
+def bestelBroodjeDoner() :
+    print("Je bestelt een broodje doner")
+
+def geenHonger() :
+    print("Je hebt geen honger")
+
+if Honger == True :
+    if zinInKoken == True :
+        goKook()
+    elif kliekje == True :
+        eetKliekje()
+    elif geld >= 10 :
+        bestelBroodjeDoner()
+    else :
+        goKook()
+else: 
+    geenHonger()
 
