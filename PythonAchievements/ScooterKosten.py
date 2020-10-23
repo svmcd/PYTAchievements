b030e80dcd81a854e72d06c89ee82d8af01fd2d5
svmcd <spolat0750@gmail.com>
2020-10-23 12:06:53 +0200

    update

--- PythonAchievements/ScooterKosten.py
+++ PythonAchievements/ScooterKosten.py
@@ -1,1 +1,21 @@
-def berkenen_maandkosten:
+
+verzekering_per_maand =  23
+benzine_kosten_per_liter =  1.54
+liter_per_kilometer = 0.2
+
+invoer = "";
+
+def bereken_maandkosten():
+    maandkosten = (km_per_maand * liter_per_kilometer * benzine_kosten_per_liter) + verzekering_per_maand
+    print("je maandkosten zijn", maandkosten)
+
+while not isinstance(invoer, float):
+
+    try: 
+        km_per_maand = input("hoeveel kilometer rijd jij per maand? ")
+        km_per_maand = float(km_per_maand)
+        bereken_maandkosten()
+        break
+
+    except ValueError:
+        print(invoer + " is geen geldig getal")
