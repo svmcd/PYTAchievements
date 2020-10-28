c53b0a6d1306e26783e6a57bec7a072166be935e
Samed <spolat0750@gmail.com>
2020-10-28 11:20:29 +0100

    Delete ShuffleAndReturn.py

--- ShuffleAndReturn.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def randomizer(woord):
-    woord = ''.join(random.sample(woord, len(woord)))
-    print(woord)
-
-randomizer("appel")
-randomizer("mediacollege")
-randomizer("olifant")
